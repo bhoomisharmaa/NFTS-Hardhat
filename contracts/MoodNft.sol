50c729098f565bc54b5e6700973505874f8e9e37
bhoomisharmaa <bhoomisharma404@gmail.com>
2025-08-19 16:26:39 +0530

    errors, events, and fixed some stuff

--- contracts/MoodNft.sol
+++ contracts/MoodNft.sol
@@ -3,9 +3,10 @@ pragma solidity 0.8.28;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
-error MoodNft__NotOwner();
+error MoodNft__NotAuthorized();
+error MoodNft__NonExistentToken();
 
-contract MoofNft is ERC721 {
+contract MoodNft is ERC721 {
     enum NftState {
         HAPPY,
         SAD
@@ -17,6 +18,14 @@ contract MoofNft is ERC721 {
 
     mapping(uint256 => NftState) s_tokenIdToNftState;
 
+    event NftMinted(address owner, uint256 tokenId);
+    event MoodFlipped(NftState currentMood, uint256 tokenId);
+
+    modifier validTokenId(uint256 tokenId) {
+        if (tokenId >= s_tokenCounter) revert MoodNft__NonExistentToken();
+        _;
+    }
+
     constructor(
         string memory happySvg,
         string memory sadSvg
@@ -27,18 +36,22 @@ contract MoofNft is ERC721 {
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToNftState[s_tokenCounter] = NftState.HAPPY;
         s_tokenCounter++;
+        emit NftMinted(msg.sender, s_tokenCounter - 1);
     }
 
-    function flipMood(uint256 tokenId) public {
-        if (
-            _getApproved(tokenId) != msg.sender &&
-            _ownerOf(tokenId) != msg.sender
-        ) revert MoodNft__NotOwner();
+    function flipMood(uint256 tokenId) public validTokenId(tokenId) {
+        address owner = _ownerOf(tokenId);
+        if (!_isAuthorized(owner, msg.sender, tokenId))
+            revert MoodNft__NotAuthorized();
 
-        if (s_tokenIdToNftState[tokenId] == NftState.HAPPY)
-            s_tokenIdToNftState[tokenId] = NftState.SAD;
-        else s_tokenIdToNftState[tokenId] = NftState.HAPPY;
+        s_tokenIdToNftState[tokenId] = s_tokenIdToNftState[tokenId] ==
+            NftState.HAPPY
+            ? NftState.SAD
+            : NftState.HAPPY;
+
+        emit MoodFlipped(s_tokenIdToNftState[tokenId], tokenId);
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -47,7 +60,14 @@ contract MoofNft is ERC721 {
 
     function tokenURI(
         uint256 tokenId
-    ) public view virtual override returns (string memory) {
+    )
+        public
+        view
+        virtual
+        override
+        validTokenId(tokenId)
+        returns (string memory)
+    {
         string memory imageURI;
         if (s_tokenIdToNftState[tokenId] == NftState.HAPPY)
             imageURI = s_happySvgImageUri;
@@ -60,7 +80,7 @@ contract MoofNft is ERC721 {
                     abi.encodePacked(
                         '{"name":"',
                         name(),
-                        '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!"',
+                        '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
                         '"attributes":[{"trait_type": "moodiness", "value": 100}], "image":"',
                         imageURI,
                         '"}'
